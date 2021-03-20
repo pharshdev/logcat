030674ea3e87aa817a31f9967aca71630b9f9aa4
Alan Trope <atrope@gmail.com>
2021-03-20 13:49:27 +0200

    update to Android v2 embed

--- android/src/main/java/dev/pharsh/logcat/LogcatPlugin.java
+++ android/src/main/java/dev/pharsh/logcat/LogcatPlugin.java
@@ -8,18 +8,30 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 
 /**
  * LogcatPlugin
  */
-public class LogcatPlugin implements MethodCallHandler {
+public class LogcatPlugin implements FlutterPlugin, MethodCallHandler {
+    private static final String CHANNEL_NAME = "app.channel.logcat";
+    private MethodChannel channel;
+
+    public LogcatPlugin() {
+    }
+
     /**
      * Plugin registration.
      */
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "app.channel.logcat");
-        channel.setMethodCallHandler(new LogcatPlugin());
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL_NAME);
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding flutterPluginBinding) {
+        channel.setMethodCallHandler(null);
     }
 
     @Override
@@ -51,4 +63,5 @@ public class LogcatPlugin implements MethodCallHandler {
             return "EXCEPTION" + e.toString();
         }
     }
+
 }
